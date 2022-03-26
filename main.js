61124b3de62e175da2af404224fbf1f02acb0c87
Endersson <enderalonso24@gmail.com>
2022-03-26 16:50:03 -0600

    Mejoras agregadas

--- main.js
+++ main.js
@@ -3,21 +3,34 @@ const calcularPension = (promedioSalario = 0, semanasCotizadas = 0) =>{
     const factorAplicacion = 0.010;
     const semanaAnual = 52;
 
-    let excesoSemanas = semanasCotizadas - 150;
-    let semanas = excesoSemanas / semanaAnual;
-    let factorAnual = semanas * factorAplicacion;
-    let tasaReemplazo = factorAnual + factorBasico;
-    let pension = tasaReemplazo * promedioSalario;
+    // primer caso
+    if(semanasCotizadas >= 750){
+        let excesoSemanas = semanasCotizadas - 150;
+        let semanas = excesoSemanas / semanaAnual;
+        let factorAnual = semanas * factorAplicacion;
+        let tasaReemplazo = factorAnual + factorBasico;
+        let pension = tasaReemplazo * promedioSalario;
+        
+        // que no sea menor de 5000
     
-    // que no sea menor de 5000
-
-    if(pension < 5000){
-        pension = 5000;
+        if(pension < 5000){
+            pension = 5000;
+        }else if(pension >= 53811.56){
+            pension = 53811.56
+        }
+    
+        return `Usted obtuvo el <span style="color:red; font-weight:bolder;">${(tasaReemplazo*100).toFixed(2)}%</span> de su salario <br> Su pensión es de: <span style="color:red;font-weight:bolder">C$ ${pension.toFixed(2)}</<span>`; ;
+    }else{
+        return '<strong>La cantidad de semanas cotizadas no es suficiente para aplicar a una pensión</strong>';
     }
-
-    return pension.toFixed(2);
 }
 
+function limpiarControles(){
+    const semanas = document.getElementById("semanas");    
+    const salario = document.getElementById("salario");
+    semanas.value = '';
+    salario.value = '';
+}
 console.log(calcularPension(150,1560))
 
 const onClickPension = () =>{
@@ -28,9 +41,10 @@ const onClickPension = () =>{
 
     const valorPension = calcularPension(salarioCot, semanaCot);
 
-    let porcentajePension = (valorPension * 100) / salarioCot;
-    
     const pensionFinal = document.getElementById("pension");
-    pensionFinal.innerHTML = `Usted obtuvo el <span style="color:red">${porcentajePension.toFixed(2)}%</span> de su salario <br> Su pensión es de: <span style="color:red;">C$ ${valorPension}</<span>`;
+    pensionFinal.innerHTML = valorPension;
+    limpiarControles();
+    
+
 }
 
