5bade23063ee004847c4cf77fed17f4274660850
Endersson <enderalonso24@gmail.com>
2022-03-30 14:02:45 -0600

    Cambios, aunque hice uso de malas practicas en codigo

--- main.js
+++ main.js
@@ -4,24 +4,40 @@ const calcularPension = (promedioSalario = 0, semanasCotizadas = 0) =>{
     const semanaAnual = 52;
 
     // primer caso
-    if(semanasCotizadas >= 750){
-        let excesoSemanas = semanasCotizadas - 150;
-        let semanas = excesoSemanas / semanaAnual;
-        let factorAnual = semanas * factorAplicacion;
-        let tasaReemplazo = factorAnual + factorBasico;
-        let pension = tasaReemplazo * promedioSalario;
-        
-        // que no sea menor de 5000
-    
-        if(pension < 5000){
-            pension = 5000;
-        }else if(pension >= 53811.56){
-            pension = 53811.56
+    if(semanasCotizadas < 750){
+        if(semanasCotizadas >= 250 && semanasCotizadas <= 349){
+            return 'Usted puede optar a una pensión minima de: <div style="color:red"><strong>C$ 1,910</strong></div>'
+        }else if(semanasCotizadas >= 350 && semanasCotizadas <= 449){
+            return 'Usted puede optar a una pensión minima de: <div style="color:red"><strong>C$ 2,356</strong></div>'
+        }else if(semanasCotizadas >= 450 && semanasCotizadas <= 549){
+            return 'Usted puede optar a una pensión minima de: <div style="color:red"><strong>C$ 2,884</strong></div>'
+        }else if(semanasCotizadas >= 550 && semanasCotizadas <= 649){
+            return 'Usted puede optar a una pensión minima de: <div style="color:red"><strong>C$ 3,290</strong></div>'
+        }else if(semanasCotizadas >= 650 && semanasCotizadas < 750){
+            return 'Usted puede optar a una pensión minima de: <div style="color:red"><strong>C$ 3,656</strong></div>'
+        }else{
+            return '<b>Lo sentimos, las semanas que usted cotizó no son suficnetes para optar a una pensión de vejez ni a pensión reducida</b>';
         }
-    
-        return `Usted obtuvo el <span style="color:red; font-weight:bolder;">${(tasaReemplazo*100).toFixed(2)}%</span> de su salario <br> Su pensión es de: <span style="color:red;font-weight:bolder">C$ ${pension.toFixed(2)}</<span>`; ;
     }else{
-        return '<strong>La cantidad de semanas cotizadas no es suficiente para aplicar a una pensión</strong>';
+        if(semanasCotizadas >= 750){
+            let excesoSemanas = semanasCotizadas - 150;
+            let semanas = excesoSemanas / semanaAnual;
+            let factorAnual = semanas * factorAplicacion;
+            let tasaReemplazo = factorAnual + factorBasico;
+            let pension = tasaReemplazo * promedioSalario;
+            
+            // que no sea menor de 5000
+        
+            if(pension < 5000){
+                pension = 5000;
+            }else if(pension >= 53811.56){
+                pension = 53811.56
+            }
+        
+            return `Usted obtuvo el <span style="color:red; font-weight:bolder;">${(tasaReemplazo*100).toFixed(2)}%</span> de su salario <br> Su pensión es de: <span style="color:red;font-weight:bolder">C$ ${pension.toFixed(2)}</<span>`; 
+        }else{
+            return '<strong>La cantidad de semanas cotizadas no es suficiente para aplicar a una pensión</strong>';
+        }
     }
 }
 
@@ -48,3 +64,9 @@ const onClickPension = () =>{
 
 }
 
+/*250-349 semanas: 1910
+  350-449: 2356
+  450-549: 2884
+  550-649:3290
+  650-749:3656
+*/
